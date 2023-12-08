636fe3190bcb28ccc73f69a8ee9206f9c01ced4d
Yohann Mansiaux <yohann@thinkr.fr>
2023-12-08 15:37:56 +0100

    02-counter wip

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,15 @@
+// set inital value to zero
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+
+$("button").on("click", function () {
+
+debugger;
+
+//$(this).attr('class');
+// si ca c'es égal à decrease alors on fait -1
+// etc
+// le compteur c'est value.innerHTML
+
+})
