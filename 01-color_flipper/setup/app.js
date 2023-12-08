6fb1799f412b2db01c5502eeaf06a1691be82b9a
Yohann Mansiaux <yohann@thinkr.fr>
2023-12-08 15:22:26 +0100

    01-color_flipper

--- 01-color_flipper/setup/app.js
+++ 01-color_flipper/setup/app.js
@@ -1,1 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+// au moment du clic sur le bouton on va tirer au hasard une couleur dans le tableau
+$("#btn").on("click", function () {
+    const randomNumber = getRandomInt(3);
+    console.log(randomNumber);
+
+    var chosenColor = colors[randomNumber];
+
+    console.log(chosenColor);
+
+    $("body").css("background-color", chosenColor);
+    $("span.color").text(chosenColor);
+    }
+);
--- 01-color_flipper/setup/hex.js
+++ 01-color_flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+$("#btn").on("click", function () {
+    var color = "#";
+
+    for (let step = 0; step < 6; step++) {
+        // Runs 5 times, with values of step 0 through 4.
+        var value = getRandomInt(hex.length);
+        color += hex[value];
+      }
+
+    $("body").css("background-color", color);
+    $("span.color").text(color);
+    }
+);
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
